ae6f67f28375716f163f067b9f580ade98313197
sdreyes <sheshechan90@gmail.com>
2018-12-07 08:59:53 -0500

    Determine if the guess is correct when an answer button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
     var correct = 0;
     var incorrect = 0;
-    var index = 0;
+    var index = 1;
     var questions = [
         {
             q: "What is 1+1?",
@@ -34,7 +34,7 @@ $(document).ready(function() {
             a2: "3",
             a3: "4",
             a4: "1,928,476",
-            answer: a1
+            answer: "a1"
         },
         {
             q: "What is yellow?",
@@ -42,24 +42,29 @@ $(document).ready(function() {
             a2: "A body part",
             a3: "A color",
             a4: "Blue",
-            answer: a3
+            answer: "a3"
         }
     ];
 
     function displayQuestion() {
         $("#question").text(questions[index].q);
         $("#a1").text(questions[index].a1);
+        $("#a1").attr("id", "a1");
         $("#a2").text(questions[index].a2);
+        $("#a2").attr("id", "a2");
         $("#a3").text(questions[index].a3);
+        $("#a3").attr("id", "a3");
         $("#a4").text(questions[index].a4);
+        $("#a4").attr("id", "a4");
     };
 
     $(".answer-button").on("click", function () {
-        var userGuess = this.text;
+        var userGuess = ($(this).attr("id"));
         timer.stop();
-        if (userGuess === questions.answer) {
-            
-        })
+        console.log(userGuess);
+        if (userGuess === questions[index].answer) {
+            console.log("true");
+        }
     })
 
     timer.start();
